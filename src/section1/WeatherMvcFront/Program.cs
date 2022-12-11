5b850118fdae25c193042cc535076e18f67b9de0
Hossein-Alizadeh <hosseinalizadeh@engineer.com>
2022-12-11 08:20:40 +0330

    Add solution in Project

--- src/section1/WeatherMvcFront/Program.cs
+++ src/section1/WeatherMvcFront/Program.cs
@@ -13,12 +13,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
 
 app.MapRazorPages();
 
